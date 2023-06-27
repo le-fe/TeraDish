f878c6c4d6312d1586c48b1011af07ab201b34f1
Toan Le <congtoanle7@gmail.com>
2023-06-27 21:05:41 +0700

    Fix backbutton

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -4,19 +4,13 @@
     class="text-4xl text-white rounded-full w-10 h-10 p-2 flex items-center justify-center transition duration-300 hover:bg-white hover:bg-opacity-20"
     @click.prevent="to ? $router.push(to) : $router.back(-1)"
   >
-    <img
-      src="@/assets/images/icons/arrow.svg"
-      alt="Go back"
-      class="block transform rotate-180"
-    />
+    <img :src="backIcon" alt="Go back" class="block transform rotate-180" />
   </button>
 </template>
 
-<script>
-export default {
-  name: "BackButton",
-  props: {
-    to: String,
-  },
-};
+<script setup lang="ts">
+const backIcon = new URL("@/assets/icons/arrow.svg", import.meta.url).href;
+defineProps({
+  to: String,
+});
 </script>
